7cd34687d2ad6e7b5fc647cfc29ec1de3a6806f7
Sharlikran <danielhmpdx16@gmail.com>
2017-10-06 16:23:41 -0700

    Cleaned up imports in Translations.py

--- Mopy/src/bolt/Translations.py
+++ Mopy/src/bolt/Translations.py
@@ -35,38 +35,37 @@ import sys
 import traceback
 import subprocess
 
-from src.bolt.Path import GPath
+from Mopy.src.bolt.Path import GPath
 
 #--Setup locale
 if locale.getlocale() == (None,None):
     locale.setlocale(locale.LC_ALL,'')
 
 # Functions --------------------------------------------------------------------
-def Dump(language,outPath,*files):
+def Dump(language, outPath, *files):
     """Dumps translatable string from *files to a new txt file in outPath,
        named based on language.  If an already existing translation file exists
        for that language, the new one will be updated with any matching strings
        from the previous one."""
-    #--Determine files to dump
+    # --Determine files to dump
     if not files:
         # No files specified.  Assume this file is located in root/src/bolt
         # and we'll dump every .py file in root recursively
         files = []
         mopy = GPath(__file__).realpath.head.head.head
-        files = [root.join(file).s for root,dirs,fnames in mopy.walk() for file in fnames
-                 if file.cext == '.py']
+        files = [root.join(file).s for root, dirs, fnames in mopy.walk() for file in fnames if file.cext == '.py']
         print('files:')
         for file in files:
             print(file)
-    #--Output files
+    # --Output files
     outTxt = '%sNEW.txt' % language
     fullTxt = outPath.join(outTxt)
     tmpTxt = outPath.join('%sNEW.tmp' % language)
     oldTxt = outPath.join('%s.txt' % language)
-    #--First dump a fresh translation file
-    args = ['p','-a','-o',fullTxt.s]
+    # --First dump a fresh translation file
+    args = ['p', '-a', '-o', fullTxt.s]
     args.extend(files)
-    if hasattr(sys,'frozen'):
+    if hasattr(sys, 'frozen'):
         # Frozen app, the tool scripts aren't accessible
         # Instead, they're included in the package as an
         # importable module
@@ -76,24 +75,25 @@ def Dump(language,outPath,*files):
         pygettext.main()
         sys.argv = old_argv
     else:
-        p = GPath(sys.prefix).join('Tools','i18n','pygettext.py')
+        p = GPath(sys.prefix).join('Tools', 'i18n', 'pygettext.py')
         args[0] = p.s
-        subprocess.call(args,shell=True)
+        subprocess.call(args, shell=True)
     return outTxt
 
-def Install(language=None,pathRead=None,pathWrite=None):
+
+def Install(language=None, pathRead=None, pathWrite=None):
     """Install translation for language.  If language is not specified,
        installs a translation for the default language."""
     if not language:
-        language = locale.getlocale()[0].split('_',1)[0]
+        language = locale.getlocale()[0].split('_', 1)[0]
     pathRead = pathRead if pathRead else GPath('l10n')
     pathWrite = pathWrite if pathWrite else pathRead
     if language.lower() == 'german':
         language = 'de'
-    txt = pathRead.join(language+'.txt')
-    po = pathWrite.join(language+'.po')
-    mo = pathWrite.join(language+'.mo')
-    #--Test for no translation for the language
+    txt = pathRead.join(language + '.txt')
+    po = pathWrite.join(language + '.po')
+    mo = pathWrite.join(language + '.mo')
+    # --Test for no translation for the language
     if not txt.exists and not mo.exists:
         if language.lower() != 'english':
             print('No translation file for language:', language)
@@ -104,8 +104,8 @@ def Install(language=None,pathRead=None,pathWrite=None):
             txtMtime = txt.mtime
             if not mo.exists or txtMtime != mo.mtime:
                 txt.copyTo(po)
-                args= ['m',po.s,'-o',mo.s]
-                if hasattr(sys,'frozen'):
+                args = ['m', po.s, '-o', mo.s]
+                if hasattr(sys, 'frozen'):
                     # Same thing as for 'Dump' for frozen
                     # apps.
                     import msgfmt
@@ -114,9 +114,9 @@ def Install(language=None,pathRead=None,pathWrite=None):
                     msgfmt.main()
                     sys.argv = old_argv
                 else:
-                    m = GPath(sys.prefix).join('Tools','i18n','msgfmt.py')
+                    m = GPath(sys.prefix).join('Tools', 'i18n', 'msgfmt.py')
                     args[0] = m.s
-                    subprocess.call(args,shell=True)
+                    subprocess.call(args, shell=True)
                 po.remove()
                 mo.mtime = txtMtime
             # Create GNU translations
