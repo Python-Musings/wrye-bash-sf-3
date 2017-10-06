9e054e699aed5af88d5c33d7568b10e53d4dbc7d
Sharlikran <danielhmpdx16@gmail.com>
2017-10-06 16:17:59 -0700

    Cleaned up bash.py errors and imports

--- Mopy/src/bash.py
+++ Mopy/src/bash.py
@@ -24,25 +24,26 @@
 """This module does the real working of starting Wrye Bash."""
 
 # Imports ----------------------------------------------------------------------
-#--Standard
+# --Standard
 import os
 import sys
 import builtins
 import traceback
-#--Local
-import src.barg as barg
-import src.bass as bass
+# --Local
+import Mopy.src.barg as barg
+import Mopy.src.bass as bass
 
 # Install dummy translation function
 builtins.__dict__['_'] = lambda x: x
+
 
 def ErrorMessage(message):
     """Show an error message using GUI if possible."""
     try:
         # Try wx first
         import wx
-        from src.balt import ShowError
-        ShowError(None,message,'Wrye Bash - Error',parseURLs=True)
+        from Mopy.src.balt import ShowError
+        ShowError(None, message, 'Wrye Bash - Error', parseURLs=True)
     except:
         # That failed, try Tkinter
         try:
@@ -50,14 +51,14 @@ def ErrorMessage(message):
             import tkinter.ttk as ttk
             root = tkinter.Tk()
             root.title('Wrye Bash')
-            frame = ttk.Frame(root,padding='3 3 3 3')
+            frame = ttk.Frame(root, padding='3 3 3 3')
             frame.pack()
             style = ttk.Style()
             style.configure('TButton')
-            button = ttk.Button(text=_('Quit'),command=root.destroy)
-            button.pack()#side=tkinter.BOTTOM)
-            text = tkinter.Text(frame,wrap='word')
-            text.insert(1.0,msg)
+            button = ttk.Button(text=_('Quit'), command=root.destroy)
+            button.pack()  # side=tkinter.BOTTOM)
+            text = tkinter.Text(frame, wrap='word')
+            text.insert(1.0, msg)
             text.config(state=tkinter.DISABLED)
             text.pack()
             root.mainloop()
@@ -69,7 +70,7 @@ def ErrorMessage(message):
 def VerifyRequirements():
     """Check to see if all required dependencies are installed."""
     errors = []
-    #--wxPython
+    # --wxPython
     try:
         import wx
         haveWx = True
@@ -77,80 +78,85 @@ def VerifyRequirements():
         errors.append(_('wxPython is required.  None detected.  Get it from:') +
                       '\n    http://wiki.wxpython.org/ProjectPhoenix')
         haveWx = False
-    #--Python 3.6
-    if sys.version_info[0:2] != (3,6):
+    # --Python 3.6
+    if sys.version_info[0:2] != (3, 6):
         url = 'https://www.python.org/downloads/release/python-363/'
         if haveWx:
-            url = '[[Python 3.6.3|'+url+']]'
+            url = '[[Python 3.6.3|' + url + ']]'
         errors.append(
-            (_('Python 3.6.3 is required.  Current version is %(version)s.  Get it from:')
-             % {'version':'.'.join(map(str,sys.version_info[0:3]))})
+            (_(
+                'Python 3.6.3 is required.  Current version is %(version)s.  Get it from:')
+             % {'version': '.'.join(map(str, sys.version_info[0:3]))})
             + '\n    %s' % url)
-    #--pywin32
+    # --pywin32
     url = 'https://sourceforge.net/projects/pywin32/files/pywin32/'
     if haveWx:
-        url = '[[pywin32 218|'+url+']]'
+        url = '[[pywin32 218|' + url + ']]'
     try:
         import win32api
-        version = win32api.GetFileVersionInfo(win32api.__file__,'\\')
+        version = win32api.GetFileVersionInfo(win32api.__file__, '\\')
         version = version['FileVersionLS'] >> 16
         if version < 218:
             errors.append(
-                (_('pywin32 218 is required.  Current version is %(version)s.  Get it from:')
-                 % {'version':version})
+                (_(
+                    'pywin32 218 is required.  Current version is %(version)s.  Get it from:')
+                 % {'version': version})
                 + '\n    %s' % url)
     except ImportError:
-        errors.append(_('pywin32 218 is required.  None detected.  Get it from:')
-                      + '\n    %s' % url)
+        errors.append(
+            _('pywin32 218 is required.  None detected.  Get it from:')
+            + '\n    %s' % url)
 
     if errors:
-        msg = (_('Cannot start Wrye Bash!  Please ensure that Python dependencies are installed correctly.')
+        msg = (_(
+            'Cannot start Wrye Bash!  Please ensure that Python dependencies are installed correctly.')
                + '\n\n' +
                '\n\n'.join(errors))
         ErrorMessage(msg)
         return False
     return True
 
+
 def main():
     try:
-        #--Parse command line
+        # --Parse command line
         barg.parse()
-        #--Initialize directories
+        # --Initialize directories
         import src.dirs
         src.dirs.InitDirs()
-        #--Setup translations
+        # --Setup translations
         try:
-            from src.bolt import Translations
+            from Mopy.src.bolt import Translations
             Translations.Install(pathRead=bass.dirs['l10n'],
-                                 pathWrite=bass.dirs['l10n.compiled'])
+                pathWrite=bass.dirs['l10n.compiled'])
         except Exception as e:
             # Translations fail.  Install a NULL Translations
             # _ function so at least we don't get errors there.
             # TODO: use logging to print
             pass
-        #--Check for dependencies
+        # --Check for dependencies
         if not VerifyRequirements():
             return
-        #--Start wxApp
+        # --Start wxApp
         import wx
         app = wx.App()
-        #--Test for single instance
-        from src.bolt import OneInstanceChecker
+        # --Test for single instance
+        from Mopy.src.bolt import OneInstanceChecker
         if bass.opts.portable:
-            oicDir = None # Use defalt
+            oicDir = None  # Use defalt
         else:
             oicDir = bass.dirs['appdata'].s
         if not OneInstanceChecker.Start(oicDir):
             return
         del OneInstanceChecker
-        #--Run the app!
+        # --Run the app!
         #  For now we're just using a dummy frame until we flesh this out
-        frame = wx.Frame(None,wx.ID_ANY,_('Haha!'))
+        frame = wx.Frame(None, wx.ID_ANY, _('Haha!'))
         frame.Show()
         frame.SetIcon(wx.Icon('bash.ico'))
         app.MainLoop()
     except Exception as e:
-        #--Something bad happened, try to show it in GUI mode.
+        # --Something bad happened, try to show it in GUI mode.
         import io
         o = io.StringIO()
         o.write(_('Could not start Wrye Bash:'))
